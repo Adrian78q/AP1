4fd3184753293fb0f9b3c137170cd10dd15fb849
adrian rodriguez <adrian78q@gmail.com>
2023-01-23 10:05:52 +0100

    Script 1

---
+++ 1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Ingresa tu nombre:"
+read nombre
+echo "Ingresa tu primer apellido:"
+read apellido1
+echo "Ingresa tu segundo apellido:"
+read apellido2
+
+sleep 5
+echo "Tu nombre completo es $nombre $apellido1 $apellido2"
