ecde2695a48f63da153fe1611876ffc5cbe64863
adrian rodriguez <adrian78q@gmail.com>
2023-01-25 09:44:14 +0100

    Script 2

---
+++ AP1-2/2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+if [ $# -ne 1 ]; then
+    echo "Uso: 2.sh [valor]"
+    exit 1
+fi
+
+valor=$1
+
+if ! [[ $valor =~ ^[0-9]+$ ]]; then
+    echo "El valor introducido no es un número."
+    exit 1
+fi
+
+if [ $valor -le 0 ]; then
+    echo "El valor introducido debe ser mayor que 0."
+    exit 1
+fi
+
+if [ $(($valor % 2)) -eq 0 ]; then
+    echo "El valor es par."
+else
+    echo "El valor es impar."
+fi
